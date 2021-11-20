9a3ce832e50ec073cc87cd715afc3091a66043de
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2021-11-20 20:37:25 +0300

    started project

---
+++ cmake-build-debug/CMakeFiles/csc215_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/csc215_project.dir/main.c.o"
+  "csc215_project"
+  "csc215_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/csc215_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
