90941eab4f7792bfbb1d65840bd757380a020197
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2021-11-20 21:37:40 +0300

    struct+txt file

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+// Attendee struct
+struct Attendee {
+    int id;
+    char *name;
+    int age;
+    char state;
+    char *address;
+    char *ActivityZone;
+    int numOfCompanion;
+    char **listOfCompanion;
+    float totalFees;
+    float balance;
+    float activityFee;
+    struct Attendee *next;
+    struct {
+        int yy;
+        int mm;
+        int dd;
+    } date[];
+};
+
 int main() {
-    printf("Hello, World!\n");
+
+    struct Attendee registered;
+    struct Attendee singleVisit;
+
     return 0;
 }
