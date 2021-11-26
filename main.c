d7f7087a5f946b9d31e55d2f58869b4e7bc29201
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2021-11-26 18:49:56 +0300

    Added method signatures + data structure methods

--- main.c
+++ main.c
@@ -29,31 +29,55 @@ typedef struct {
     float totalFees;
     float balance;
     float activityFee;
-    struct Date date[];
+    struct Date *date;
 } Attendee;
 
 
-struct activity {
+typedef struct {
     char name[35];
     float price;
     Date startDate;
     Date endDate;
     int freePassAge;
     int ageRestriction;
-};
+
+} activity;
 
 struct node {
     Attendee val;
     struct node *next;
 };
 
-void readFile();
+void readFile(); //Heila Al-Mogren
+void showMainMenu(); //Alanoud Al-ashSheikh
+void addNewAttendee(); //Alanoud KG
+void editRecord();
+
+void editActivity();
+
+void showAttendeeReport();
+
+void showAttendee();
+
+void deleteRecord();
+
+void writeAttendee(char *fileName);
+
+void printList();
+
+// Data structures
+void deleteNode(struct node **head, int id);
+void printList(struct node * head);
+void insertAtEnd(struct node **head, Attendee attendee);
+void insertAtBeginning(struct node **head, Attendee attendee);
 
 
 int main() {
 
-    struct node *registered_head;
+    struct node *registered_head, *prev, *cur;
     registered_head = (struct node *) malloc(sizeof(struct node));
+
+
     Attendee registered;
     registered_head->val = registered;
 
@@ -61,6 +85,7 @@ int main() {
     singleVisit_head = (struct node *) malloc(sizeof(struct node));
 
     // allocate memory for activity list (array)
+    activity *ActivitiesList = (activity *) calloc(10, sizeof(activity));
 
     // read file and fill activity list
     readFile();
@@ -70,4 +95,63 @@ int main() {
     return 0;
 }
 
+void readFile() {
 
+}
+
+void showMainMenu() {
+
+}
+
+void insertAtBeginning(struct node **head, Attendee attendee) {
+    struct node *newNode = (struct node *) malloc(sizeof(struct node));
+    newNode->val = attendee;
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void insertAtEnd(struct node **head, Attendee attendee){
+    if (*head == NULL) { //if the list is empty
+        *head = (struct node *) malloc(sizeof(struct node));
+        (*head)->val = attendee;
+        (*head)->next = NULL; }
+    else { //if the list has at least one node
+        struct node * current = *head;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+/* now we can add a new variable */
+        current->next = (struct node *)malloc(sizeof(struct node));
+        current->next->val = attendee;
+        current->next->next = NULL;
+    } }
+
+
+void printList(struct node * head) {
+    struct node * current = head;
+    while(current != NULL)
+    {
+        printf("%d->",current->val); // edit this to show the details of attendee
+        current = current ->next;
+    }
+}
+
+
+void deleteNode(struct node **head, int id) {
+    if (*head != NULL) {
+        if ((*head)->val.id == id) { //if the deleted node is the first node
+            struct node * temp = *head;
+            *head = (*head)->next;
+            free(temp);
+        }
+        else {
+            struct node *prev = *head;
+            struct node *cur = (*head)->next ;
+            while (cur != NULL) {
+                if (cur->val.id == id) {
+                    prev->next = cur->next;
+                    free(cur);
+                    break; } //exit loop when delete the node
+                prev = cur;
+                cur = cur ->next;
+            } } } }
