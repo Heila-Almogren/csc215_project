7317858ef75b1a7d78a96eba23813eaea1c79f67
Heila Al-Mogren <55871567+Heila-Almogren@users.noreply.github.com>
2021-11-29 17:57:26 +0300

    added read file

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
    #include <stdlib.h>
    #include <string.h>
    #include <stdbool.h>
-   
+   #include <ctype.h>
+
    typedef struct {
        int id;
        char name[20];
@@ -47,6 +48,7 @@
    
       
    void readFile(); //Heila Al-Mogren
+   void test_readFile(activity activities[]);
    void showMainMenu(); //Alanoud Al-ashSheikh
    void addNewAttendee(); //Alanoud KG
    void editRecord();
@@ -160,10 +162,164 @@
 
        
    }//end addNewAttendee
-   
-   
+
+
    void readFile() {
-   
+
+
+       activity *activities = NULL;
+       int nActivities = 0;
+
+       FILE *fp;
+       fp = fopen("../Activities.txt", "r");
+       char tx[200];
+       char name[35];
+       double price = 0;
+//    Date startDate;
+//    Date endDate;
+       int freePassAge = 0;
+       int ageRestriction = 0;
+       char fullDate[50];
+
+       char startDate_dd[33];
+       char startDate_mm[33];
+       char startDate_yy[33];
+       char endDate_dd[33];
+       char endDate_mm[33];
+       char endDate_yy[33];
+
+
+       char *eof;
+       int line_counter = 0;
+
+       // Skip first two lines
+       eof = fgets(tx, 200, fp);
+       eof = fgets(tx, 200, fp);
+
+       while (eof) {
+
+           activities = realloc(activities, (nActivities + 1) * sizeof(activity));
+           activity ac;
+
+           // Get name
+           eof = fgets(tx, 33, fp);
+           sscanf(tx, "%[^\\n]s", name);
+           // Remove extra space
+           char *last = name + strlen(name);
+           while (isspace(*--last));
+           *(last + 1) = '\0';
+           strcpy(ac.name, name);
+
+           // Get Price
+           eof = fgets(tx, 18, fp);
+           sscanf(tx, "%lf", &price);
+           ac.price = price;
+
+           // Get date
+           eof = fgets(tx, 32, fp);
+           printf("Date: %s\n", tx);
+           sscanf(tx, "%s", fullDate);
+           printf("Full date is is %s\n", fullDate);
+//        2021/11/01-2022/04/01
+
+           sscanf(tx, "%s", fullDate);
+           //start date (day)
+           strcpy(startDate_dd, tx);
+
+           *(startDate_dd + 4) = '\0';
+           sscanf(startDate_dd, "%d", &ac.startDate.dd);
+
+           //start date (month)
+           strcpy(startDate_mm, tx + 5);
+           *(startDate_mm + 2) = '\0';
+           sscanf(startDate_mm, "%d", &ac.startDate.mm);
+
+           //start date (year)
+           strcpy(startDate_yy, tx + 8);
+           *(startDate_yy + 2) = '\0';
+           sscanf(startDate_yy, "%d", &ac.startDate.yy);
+
+
+           //end date (day)
+           strcpy(endDate_dd, tx+11);
+           *(endDate_dd + 4) = '\0';
+           sscanf(endDate_dd, "%d", &ac.endDate.dd);
+
+           //end date (month)
+           strcpy(endDate_mm, tx + 16);
+           *(endDate_mm + 2) = '\0';
+           sscanf(endDate_mm, "%d", &ac.endDate.mm);
+
+           //end date (year)
+           strcpy(endDate_yy, tx + 19);
+           *(endDate_yy + 2) = '\0';
+           sscanf(endDate_yy, "%d", &ac.endDate.yy);
+
+
+
+
+
+           // ignore the <
+           eof = fgets(tx, 2, fp);
+           printf("Ignored sign: %s\n", tx);
+
+           if (strcmp(tx, "-") == 0) {
+               freePassAge = -1;
+               eof = fgets(tx, 19, fp);
+           } else {
+               // Get FREEPASS
+               eof = fgets(tx, 19, fp);
+               printf("Free pass: %s\n", tx);
+               sscanf(tx, "%d", &freePassAge);
+           }
+
+           // ignore the > or
+           eof = fgets(tx, 2, fp);
+           printf("Ignored sign: %s\n", tx);
+
+           if (strcmp(tx, "-") == 0) {
+               ageRestriction = -1;
+               eof = fgets(tx, 20, fp);
+           } else {
+               // Get minimum age
+               eof = fgets(tx, 20, fp);
+               printf("Min Age: %s\n", tx);
+               sscanf(tx, "%d", &ageRestriction);
+           }
+
+           line_counter++;
+
+
+           ac.freePassAge = freePassAge;
+           ac.ageRestriction = ageRestriction;
+
+           *(activities + nActivities) = ac;
+           nActivities++;
+       };
+
+       fclose(fp);
+
+       test_readFile(activities);
+   }
+
+   void test_readFile(activity activities[]) {
+
+       for (int i = 0; i < 10; i++) {
+           printf("%d | Name: %-32s\t Price: %-18lf\t Date:%d/%d/%d-%d/%d/%d \t freePassAge:%d\t ageRestriction: %d\n",
+                  i + 1,
+                  activities[i].name,
+                  activities[i].price,
+                  activities[i].startDate.dd,
+                  activities[i].startDate.mm,
+                  activities[i].startDate.yy,
+                  activities[i].endDate.dd,
+                  activities[i].endDate.mm,
+                  activities[i].endDate.yy,
+                  activities[i].freePassAge,
+                  activities[i].ageRestriction
+           );
+
+       }
    }
    
    void showMainMenu() {
